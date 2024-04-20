347666c238b87b4a315680a5081b556cec3750e7
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-04-20 21:04:16 +0900

    height, weight を print

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -12,15 +12,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     @IBAction func heightSliderChanged(_ sender: UISlider) {
-        print(sender.value)
+        print(String(format: "%.2f", sender.value))
     }
     
     @IBAction func weightSliderChanged(_ sender: UISlider) {
-        print(sender.value)
+        print(Int(sender.value))
     }
 }
 
