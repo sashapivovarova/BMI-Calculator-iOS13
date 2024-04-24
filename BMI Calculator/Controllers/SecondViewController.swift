ac13b71d8fd349a78d00b83cc033d8cd13be44fd
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-04-24 21:38:12 +0900

    SecondViewController の追加

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondView.swift
+//  BMI Calculator
+//
+//  Created by Anastasia Grey on 2024/04/23.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
